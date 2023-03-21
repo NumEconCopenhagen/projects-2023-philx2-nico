4d3412c1fd1574a2473a342be2c4d1a6f35f8f67
lqz982 <lqz982@alumni.ku.dk>
2023-03-21 15:26:34 +0100

    copypaste

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,3 +1,12 @@
+
+from types import SimpleNamespace
+
+import numpy as np
+from scipy import optimize
+
+import pandas as pd 
+import matplotlib.pyplot as plt
+
 class HouseholdSpecializationModelClass:
 
     def __init__(self):
@@ -5,6 +14,7 @@ class HouseholdSpecializationModelClass:
         
         # a. create namespaces
         par = self.par = SimpleNamespace()
+        
         sol = self.sol = SimpleNamespace()
 
         # b. preferences
@@ -14,12 +24,31 @@ class HouseholdSpecializationModelClass:
         par.omega = 0.5 
 
         # c. household production
-        par.alpha = [0.25, 0.50, 0.75]
-        par.sigma = [0.5, 1.0, 1.50]
+        par.alpha = 0.5
+        par.sigma = 1.0
 
+
+        alpha_values = [0.25, 0.5, 0.75]
+        sigma_values = [0.5, 1.0, 1.5]
+        
         # d. wages
         par.wM = 1.0
         par.wF = 1.0
+        par.wF_vec = np.linspace(0.8,1.2,5)
+
+        # e. targets
+        par.beta0_target = 0.4
+        par.beta1_target = -0.1
+
+        # f. solution
+        sol.LM_vec = np.zeros(par.wF_vec.size)
+        sol.HM_vec = np.zeros(par.wF_vec.size)
+        sol.LF_vec = np.zeros(par.wF_vec.size)
+        sol.HF_vec = np.zeros(par.wF_vec.size)
+
+        sol.beta0 = np.nan
+        sol.beta1 = np.nan
+
 
         
 
