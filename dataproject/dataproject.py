a05266ec4398b8eb6c3e88c69b0a5013edba5418
lxg969 <lxg969@alumni.ku.dk>
2023-04-11 15:17:25 +0200

    Jeg har tilføjet scraper klassen med 2 fkt.

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -9,8 +9,16 @@ def keep_regs(df, regs):
 
     """ 
     
-    for r in regs:
-        I = df.reg.str.contains(r)
-        df = df.loc[I == False] # keep everything else
-    
+        # Export the DataFrame to an Excel file
+        if file_name is None:
+            if property_type is None:
+                file_name = f'scraped_data.xlsx'
+            else:
+                file_name = f'scraped_data_{property_type}.xlsx'
+        else:
+            if property_type is None:
+                file_name = f'scraped_data_{file_name}.xlsx'
+            else:
+                file_name = f'scraped_data_{property_type}_{file_name}.xlsx'
+        df.to_excel(file_name, index=False)
     return df
