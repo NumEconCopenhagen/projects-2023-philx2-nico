3dbe0d01bbe3c147ea96a33997139223e83b2eb3
qfx241 <qfx241@alumni.ku.dk>
2023-05-25 09:09:13 +0200

    1

---
+++ Exam/Exam.py
@@ -0,0 +1,16 @@
+Inaugural project
+
+This project is made by Philip Gade Reumert, Philip Engel and Nicolai Møller.
+
+The outcome of this project can be observed by from running our inauguralproject.ipynb file.
+
+To succesfully run this file it is reguired to run the inauguralproject.py file.
+
+Dependencies: In order to effectively execute the code, the specific dependencies must be installed:
+
+numpy
+scipy
+pandas
+matplotlib
+seaborn
+Please ensure these dependencies are installed in your Python environment to enable the flawless running of the code.
